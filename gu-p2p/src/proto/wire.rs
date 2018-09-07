d7e730b518e97a23fb7e1f2eb4833e8a207f14ba
Przemysław K. Rekucki <przemyslaw.rekucki@golem.network>
2018-09-07 18:51:52 +0200

    Changed ws protocool negotiation

--- gu-p2p/src/proto/wire.rs
+++ gu-p2p/src/proto/wire.rs
@@ -50,12 +50,52 @@ impl<'a> From<&'a str> for Role {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum RpcStatus {
+    Success = 0,
+    NoDestination = 1,
+    BadFormat = 2,
+}
+
+impl Default for RpcStatus {
+    fn default() -> Self {
+        RpcStatus::Success
+    }
+}
+
+impl From<i32> for RpcStatus {
+    fn from(i: i32) -> Self {
+        match i {
+            0 => RpcStatus::Success,
+            1 => RpcStatus::NoDestination,
+            2 => RpcStatus::BadFormat,
+            _ => Self::default(),
+        }
+    }
+}
+
+impl<'a> From<&'a str> for RpcStatus {
+    fn from(s: &'a str) -> Self {
+        match s {
+            "Success" => RpcStatus::Success,
+            "NoDestination" => RpcStatus::NoDestination,
+            "BadFormat" => RpcStatus::BadFormat,
+            _ => Self::default(),
+        }
+    }
+}
+
 #[derive(Debug, Default, PartialEq, Clone)]
 pub struct Hello<'a> {
     pub role: Role,
-    pub node_name: Cow<'a, str>,
+    pub node_name: Option<Cow<'a, str>>,
     pub node_id: Cow<'a, [u8]>,
-    pub version: Vec<u32>,
+    pub instance_id: Cow<'a, [u8]>,
+    pub version: Option<Cow<'a, str>>,
+    pub os: Option<Cow<'a, str>>,
+    pub max_ram: Option<u64>,
+    pub max_storage: Option<u64>,
+    pub exec_envs: Vec<Cow<'a, str>>,
 }
 
 impl<'a> MessageRead<'a> for Hello<'a> {
@@ -64,9 +104,14 @@ impl<'a> MessageRead<'a> for Hello<'a> {
         while !r.is_eof() {
             match r.next_tag(bytes) {
                 Ok(8) => msg.role = r.read_enum(bytes)?,
-                Ok(18) => msg.node_name = r.read_string(bytes).map(Cow::Borrowed)?,
+                Ok(18) => msg.node_name = Some(r.read_string(bytes).map(Cow::Borrowed)?),
                 Ok(26) => msg.node_id = r.read_bytes(bytes).map(Cow::Borrowed)?,
-                Ok(32) => msg.version.push(r.read_uint32(bytes)?),
+                Ok(34) => msg.instance_id = r.read_bytes(bytes).map(Cow::Borrowed)?,
+                Ok(42) => msg.version = Some(r.read_string(bytes).map(Cow::Borrowed)?),
+                Ok(82) => msg.os = Some(r.read_string(bytes).map(Cow::Borrowed)?),
+                Ok(88) => msg.max_ram = Some(r.read_uint64(bytes)?),
+                Ok(96) => msg.max_storage = Some(r.read_uint64(bytes)?),
+                Ok(106) => msg.exec_envs.push(r.read_string(bytes).map(Cow::Borrowed)?),
                 Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
@@ -79,17 +124,154 @@ impl<'a> MessageWrite for Hello<'a> {
     fn get_size(&self) -> usize {
         0
         + 1 + sizeof_varint(*(&self.role) as u64)
-        + 1 + sizeof_len((&self.node_name).len())
+        + self.node_name.as_ref().map_or(0, |m| 1 + sizeof_len((m).len()))
         + 1 + sizeof_len((&self.node_id).len())
-        + self.version.iter().map(|s| 1 + sizeof_varint(*(s) as u64)).sum::<usize>()
+        + 1 + sizeof_len((&self.instance_id).len())
+        + self.version.as_ref().map_or(0, |m| 1 + sizeof_len((m).len()))
+        + self.os.as_ref().map_or(0, |m| 1 + sizeof_len((m).len()))
+        + self.max_ram.as_ref().map_or(0, |m| 1 + sizeof_varint(*(m) as u64))
+        + self.max_storage.as_ref().map_or(0, |m| 1 + sizeof_varint(*(m) as u64))
+        + self.exec_envs.iter().map(|s| 1 + sizeof_len((s).len())).sum::<usize>()
     }
 
     fn write_message<W: Write>(&self, w: &mut Writer<W>) -> Result<()> {
         w.write_with_tag(8, |w| w.write_enum(*&self.role as i32))?;
-        w.write_with_tag(18, |w| w.write_string(&**&self.node_name))?;
+        if let Some(ref s) = self.node_name { w.write_with_tag(18, |w| w.write_string(&**s))?; }
         w.write_with_tag(26, |w| w.write_bytes(&**&self.node_id))?;
-        for s in &self.version { w.write_with_tag(32, |w| w.write_uint32(*s))?; }
+        w.write_with_tag(34, |w| w.write_bytes(&**&self.instance_id))?;
+        if let Some(ref s) = self.version { w.write_with_tag(42, |w| w.write_string(&**s))?; }
+        if let Some(ref s) = self.os { w.write_with_tag(82, |w| w.write_string(&**s))?; }
+        if let Some(ref s) = self.max_ram { w.write_with_tag(88, |w| w.write_uint64(*s))?; }
+        if let Some(ref s) = self.max_storage { w.write_with_tag(96, |w| w.write_uint64(*s))?; }
+        for s in &self.exec_envs { w.write_with_tag(106, |w| w.write_string(&**s))?; }
         Ok(())
     }
+}
+
+#[derive(Debug, Default, PartialEq, Clone)]
+pub struct HelloReply<'a> {
+    pub role: Role,
+    pub node_name: Option<Cow<'a, str>>,
+    pub node_id: Cow<'a, [u8]>,
+    pub version: Option<Cow<'a, str>>,
+    pub max_ping_ms: Option<i32>,
+}
+
+impl<'a> MessageRead<'a> for HelloReply<'a> {
+    fn from_reader(r: &mut BytesReader, bytes: &'a [u8]) -> Result<Self> {
+        let mut msg = Self::default();
+        while !r.is_eof() {
+            match r.next_tag(bytes) {
+                Ok(8) => msg.role = r.read_enum(bytes)?,
+                Ok(18) => msg.node_name = Some(r.read_string(bytes).map(Cow::Borrowed)?),
+                Ok(26) => msg.node_id = r.read_bytes(bytes).map(Cow::Borrowed)?,
+                Ok(34) => msg.version = Some(r.read_string(bytes).map(Cow::Borrowed)?),
+                Ok(160) => msg.max_ping_ms = Some(r.read_int32(bytes)?),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
+                Err(e) => return Err(e),
+            }
+        }
+        Ok(msg)
+    }
+}
+
+impl<'a> MessageWrite for HelloReply<'a> {
+    fn get_size(&self) -> usize {
+        0
+        + 1 + sizeof_varint(*(&self.role) as u64)
+        + self.node_name.as_ref().map_or(0, |m| 1 + sizeof_len((m).len()))
+        + 1 + sizeof_len((&self.node_id).len())
+        + self.version.as_ref().map_or(0, |m| 1 + sizeof_len((m).len()))
+        + self.max_ping_ms.as_ref().map_or(0, |m| 2 + sizeof_varint(*(m) as u64))
+    }
+
+    fn write_message<W: Write>(&self, w: &mut Writer<W>) -> Result<()> {
+        w.write_with_tag(8, |w| w.write_enum(*&self.role as i32))?;
+        if let Some(ref s) = self.node_name { w.write_with_tag(18, |w| w.write_string(&**s))?; }
+        w.write_with_tag(26, |w| w.write_bytes(&**&self.node_id))?;
+        if let Some(ref s) = self.version { w.write_with_tag(34, |w| w.write_string(&**s))?; }
+        if let Some(ref s) = self.max_ping_ms { w.write_with_tag(160, |w| w.write_int32(*s))?; }
+        Ok(())
+    }
+}
+
+#[derive(Debug, Default, PartialEq, Clone)]
+pub struct RpcMessage<'a> {
+    pub message_id: Cow<'a, [u8]>,
+    pub destination_id: Cow<'a, [u8]>,
+    pub correlation_id: Option<Cow<'a, [u8]>>,
+    pub ts: Option<u64>,
+    pub expires: Option<u64>,
+    pub status: RpcStatus,
+    pub payload: mod_RpcMessage::OneOfpayload<'a>,
+}
+
+impl<'a> MessageRead<'a> for RpcMessage<'a> {
+    fn from_reader(r: &mut BytesReader, bytes: &'a [u8]) -> Result<Self> {
+        let mut msg = Self::default();
+        while !r.is_eof() {
+            match r.next_tag(bytes) {
+                Ok(10) => msg.message_id = r.read_bytes(bytes).map(Cow::Borrowed)?,
+                Ok(18) => msg.destination_id = r.read_bytes(bytes).map(Cow::Borrowed)?,
+                Ok(26) => msg.correlation_id = Some(r.read_bytes(bytes).map(Cow::Borrowed)?),
+                Ok(80) => msg.ts = Some(r.read_uint64(bytes)?),
+                Ok(88) => msg.expires = Some(r.read_uint64(bytes)?),
+                Ok(32) => msg.status = r.read_enum(bytes)?,
+                Ok(162) => msg.payload = mod_RpcMessage::OneOfpayload::json(r.read_string(bytes).map(Cow::Borrowed)?),
+                Ok(170) => msg.payload = mod_RpcMessage::OneOfpayload::error_msg(r.read_string(bytes).map(Cow::Borrowed)?),
+                Ok(t) => { r.read_unknown(bytes, t)?; }
+                Err(e) => return Err(e),
+            }
+        }
+        Ok(msg)
+    }
+}
+
+impl<'a> MessageWrite for RpcMessage<'a> {
+    fn get_size(&self) -> usize {
+        0
+        + 1 + sizeof_len((&self.message_id).len())
+        + 1 + sizeof_len((&self.destination_id).len())
+        + self.correlation_id.as_ref().map_or(0, |m| 1 + sizeof_len((m).len()))
+        + self.ts.as_ref().map_or(0, |m| 1 + sizeof_varint(*(m) as u64))
+        + self.expires.as_ref().map_or(0, |m| 1 + sizeof_varint(*(m) as u64))
+        + 1 + sizeof_varint(*(&self.status) as u64)
+        + match self.payload {
+            mod_RpcMessage::OneOfpayload::json(ref m) => 2 + sizeof_len((m).len()),
+            mod_RpcMessage::OneOfpayload::error_msg(ref m) => 2 + sizeof_len((m).len()),
+            mod_RpcMessage::OneOfpayload::None => 0,
+    }    }
+
+    fn write_message<W: Write>(&self, w: &mut Writer<W>) -> Result<()> {
+        w.write_with_tag(10, |w| w.write_bytes(&**&self.message_id))?;
+        w.write_with_tag(18, |w| w.write_bytes(&**&self.destination_id))?;
+        if let Some(ref s) = self.correlation_id { w.write_with_tag(26, |w| w.write_bytes(&**s))?; }
+        if let Some(ref s) = self.ts { w.write_with_tag(80, |w| w.write_uint64(*s))?; }
+        if let Some(ref s) = self.expires { w.write_with_tag(88, |w| w.write_uint64(*s))?; }
+        w.write_with_tag(32, |w| w.write_enum(*&self.status as i32))?;
+        match self.payload {            mod_RpcMessage::OneOfpayload::json(ref m) => { w.write_with_tag(162, |w| w.write_string(&**m))? },
+            mod_RpcMessage::OneOfpayload::error_msg(ref m) => { w.write_with_tag(170, |w| w.write_string(&**m))? },
+            mod_RpcMessage::OneOfpayload::None => {},
+    }        Ok(())
+    }
+}
+
+pub mod mod_RpcMessage {
+
+use super::*;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum OneOfpayload<'a> {
+    json(Cow<'a, str>),
+    error_msg(Cow<'a, str>),
+    None,
+}
+
+impl<'a> Default for OneOfpayload<'a> {
+    fn default() -> Self {
+        OneOfpayload::None
+    }
+}
+
 }
 
--- gu-p2p/src/rpc/ws.rs
+++ gu-p2p/src/rpc/ws.rs
@@ -2,5 +2,124 @@
  * Websocket interface for message router
  */
 
+use super::super::proto::wire;
+use super::message::{NodeId, RouteMessage, TransportError, TransportResult, MessageId};
+use super::router::MessageRouter;
 use actix::prelude::*;
-use actix_web::*;
+use actix_web::{self, ws, HttpRequest, HttpResponse};
+use std::marker::PhantomData;
+use quick_protobuf::serialize_into_vec;
+
+struct Worker<S: 'static> {
+    state: PhantomData<S>,
+    node_id : NodeId,
+    peer_node_id: Option<NodeId>,
+}
+
+impl<S> Worker<S> {
+    fn new(node_id : NodeId) -> Self {
+        Worker {
+            state: PhantomData,
+            node_id,
+            peer_node_id: None,
+        }
+    }
+
+    fn route(&mut self, rpc: wire::RpcMessage) {
+        let body: String = match (rpc.status, rpc.payload) {
+            (wire::RpcStatus::Success, wire::mod_RpcMessage::OneOfpayload::json(json)) => {
+                json.into()
+            }
+            (wire::RpcStatus::NoDestination, _) => {
+                //TransportResult::Err(TransportError::NoDestination)
+                return ()
+            }
+            (wire::RpcStatus::BadFormat, wire::mod_RpcMessage::OneOfpayload::error_msg(msg)) => {
+                //TransportResult::Err(TransportError::BadFormat(msg.into()))
+                return ()
+            }
+            _ => return (),
+        };
+
+        MessageRouter::from_registry().do_send(RouteMessage {
+            msg_id: rpc.message_id.as_ref().into(),
+            sender: self.peer_node_id.clone().unwrap(),
+            destination: rpc.destination_id.as_ref().into(),
+            reply_to: None,
+            correlation_id: None,
+            ts: rpc.ts.unwrap_or(0),
+            expires: rpc.expires,
+            body,
+        })
+    }
+
+    fn reply_init(&mut self, ctx : &mut <Self as Actor>::Context) {
+        use std::borrow::Cow;
+
+        let hello = wire::HelloReply {
+            role: wire::Role::HUB,
+            node_name: None,
+            node_id: Cow::Borrowed(self.node_id.as_ref()),
+            version: Some(Cow::Borrowed("0.1")),
+            max_ping_ms: None,
+        };
+
+        let v = serialize_into_vec(&hello).expect("Cannot write message!");
+
+        ctx.binary(v);
+    }
+}
+
+impl<S> Actor for Worker<S> {
+    type Context = ws::WebsocketContext<Self, S>;
+
+    fn started(&mut self, ctx: &mut <Self as Actor>::Context) {
+
+    }
+}
+
+impl<S> StreamHandler<ws::Message, ws::ProtocolError> for Worker<S> {
+    fn handle(&mut self, item: ws::Message, ctx: &mut Self::Context) {
+        use quick_protobuf::{BytesReader, MessageRead};
+
+        match item {
+            ws::Message::Binary(b) => if self.peer_node_id.is_none() {
+                let mut reader = BytesReader::from_bytes(b.as_ref());
+                match wire::Hello::from_reader(&mut reader, b.as_ref()) {
+                    Ok(hello) => {
+                        info!("handshake for: {:?}", hello);
+                        let mut peer_node_id:NodeId = NodeId::default();
+                        peer_node_id[..].copy_from_slice(hello.node_id.as_ref());
+                        self.peer_node_id = Some(peer_node_id);
+                        self.reply_init(ctx)
+                    }
+                    Err(e) => {
+                        ctx.close(Some(ws::CloseReason {
+                            code: ws::CloseCode::Protocol,
+                            description: Some(format!("{}", e)),
+                        }));
+                    }
+                }
+            } else {
+                let mut reader = BytesReader::from_bytes(b.as_ref());
+                match wire::RpcMessage::from_reader(&mut reader, b.as_ref()) {
+                    Ok(rpc) => self.route(rpc),
+                    Err(e) => {
+                        ctx.close(Some(ws::CloseReason {
+                            code: ws::CloseCode::Protocol,
+                            description: Some(format!("{}", e)),
+                        }));
+                    }
+                }
+            },
+            p => {
+                warn!("unknown package: {:?}", p)
+            },
+        }
+    }
+}
+
+pub fn route<T: 'static>(req: HttpRequest<T>, node_id : NodeId) -> Result<HttpResponse, actix_web::Error> {
+    let actor = Worker::new(node_id);
+    ws::start(&req, actor)
+}
