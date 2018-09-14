7971e55e3fb7c1cabb1de7f821fe6a21b1f6b6fb
Przemysław K. Rekucki <przemyslaw.rekucki@golem.network>
2018-09-14 17:24:16 +0200

    Added smiple ui

---
+++ gu-hub/build.rs
@@ -0,0 +1,24 @@
+
+use std::{env, fs, path};
+use std::io::prelude::*;
+//use flate2;
+
+fn main() {
+    let mut outf = fs::OpenOptions::new()
+        .read(false)
+        .write(true)
+        .create(true).append(true)
+        .open("/tmp/build.log").unwrap();
+
+    writeln!(outf, "start!");
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_path : path::PathBuf = out_dir.into();
+
+    let webapp = fs::read_dir("webapp").unwrap();
+
+    for f in webapp {
+        let entry = f.unwrap();
+        writeln!(outf, "entry: {:?}", entry.path());
+    }
+
+}
