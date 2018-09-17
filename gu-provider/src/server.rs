9d40159234c0680c2cc28ab9e2b1cb985ed1053c
Piotr Chromiec <tworec@golem.network>
2018-09-17 18:08:56 +0200

    [gu-provider] remember hub connection

--- gu-provider/src/server.rs
+++ gu-provider/src/server.rs
@@ -1,35 +1,33 @@
 use actix::fut;
 use actix::prelude::*;
-//use futures::future;
-use futures::prelude::*;
-//use tokio;
-
-use gu_persist::config;
-
-//use actix_web::server::HttpServer;
-use actix_web::server::StopServer;
 use actix_web::*;
 use clap::{self, Arg, ArgMatches, SubCommand};
+use futures::prelude::*;
 use std::borrow::Cow;
-use std::net::{self, ToSocketAddrs};
+use std::net::{SocketAddr, ToSocketAddrs};
 use std::sync::Arc;
 
 use gu_base::Decorator;
 use gu_base::Module;
-use gu_p2p::rpc;
-use gu_p2p::NodeId;
-use gu_persist::config::ConfigModule;
 use gu_ethkey::{EthKey, EthKeyStore, SafeEthKey};
-use mdns::{Responder, Service};
+use gu_p2p::rpc;
+use gu_p2p::rpc::ws::ConnectionSupervisor;
+use gu_p2p::NodeId;
+use gu_persist::config::{
+    ConfigManager, ConfigModule, Error as ConfigError, GetConfig, HasSectionId, SetConfig,
+    SetConfigPath,
+};
+use mdns::Responder;
 use std::path::PathBuf;
 
-
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 struct ServerConfig {
     p2p_port: u16,
     #[serde(skip_serializing_if = "Option::is_none")]
     control_socket: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    hub_addr: Option<SocketAddr>,
 }
 
 impl Default for ServerConfig {
@@ -37,6 +35,7 @@ impl Default for ServerConfig {
         ServerConfig {
             p2p_port: 61621,
             control_socket: None,
+            hub_addr: None,
         }
     }
 }
@@ -47,14 +46,14 @@ impl ServerConfig {
     }
 }
 
-impl config::HasSectionId for ServerConfig {
+impl HasSectionId for ServerConfig {
     const SECTION_ID: &'static str = "provider-server-cfg";
 }
 
 pub struct ServerModule {
     active: bool,
     config_path: Option<String>,
-    peer_addr: Option<net::SocketAddr>,
+    hub_addr: Option<SocketAddr>,
 }
 
 impl ServerModule {
@@ -62,16 +61,13 @@ impl ServerModule {
         ServerModule {
             active: false,
             config_path: None,
-            peer_addr: None,
+            hub_addr: None,
         }
     }
 }
 
-fn get_node_id<P: Into<PathBuf>>(keystore_path: P) -> NodeId {
-    let keys = SafeEthKey::load_or_generate(keystore_path, &"".into()).unwrap();
-    let mut public_key_bytes: &[u8] = keys.public().as_ref();
-    // TODO: NodeId 32 --> 64 ?
-    let node_id = NodeId::from(&public_key_bytes[0..32]);
+fn get_node_id(keys: Box<SafeEthKey>) -> NodeId {
+    let node_id = NodeId::from(keys.address().as_ref());
     info!("node_id={:?}", node_id);
     node_id
 }
@@ -81,7 +77,7 @@ impl Module for ServerModule {
         app.subcommand(
             SubCommand::with_name("server")
                 .about("provider server management")
-                .subcommand(SubCommand::with_name("connect").arg(Arg::with_name("peer_addr"))),
+                .subcommand(SubCommand::with_name("connect").arg(Arg::with_name("hub_addr"))),
         )
     }
 
@@ -92,10 +88,10 @@ impl Module for ServerModule {
             println!("server");
             self.active = true;
             if let Some(mc) = m.subcommand_matches("connect") {
-                let param = mc.value_of("peer_addr");
-                info!("peer addr={:?}", &param);
+                let param = mc.value_of("hub_addr");
+                info!("hub addr={:?}", &param);
                 if let Some(addr) = param {
-                    self.peer_addr = Some(addr.parse().unwrap())
+                    self.hub_addr = Some(addr.parse().unwrap())
                 }
             }
             return true;
@@ -105,25 +101,24 @@ impl Module for ServerModule {
 
     fn run<D: Decorator + Clone + 'static>(&self, decorator: D) {
         use actix;
-        //    use env_logger;
-        use rand::*;
 
         if !self.active {
             return;
         }
 
-        let config = ServerConfigurer(None, self.config_path.clone()).start();
+        let config_module: &ConfigModule = decorator.extract().unwrap();
+        // TODO: introduce separate actor for key mgmt
+        let keys = SafeEthKey::load_or_generate(config_module.keystore_path(), &"".into()).unwrap();
+
+        let _ = ServerConfigurer {
+            config_path: self.config_path.clone(),
+            node_id: get_node_id(keys),
+            hub_addr: self.hub_addr,
+        }.start();
+
+        let _ = super::hdman::start(config_module);
+
         let sys = actix::System::new("gu-provider");
-
-        let configModule: &ConfigModule = decorator.extract().unwrap();
-        let _ = super::hdman::start(configModule);
-
-
-
-        if let Some(a) = self.peer_addr {
-            let _ = rpc::ws::start_connection(get_node_id(configModule.keystore_path()), a);
-        }
-
         let _ = sys.run();
     }
 }
@@ -145,27 +140,33 @@ fn run_mdns_publisher(port: u16) {
     let _ = Box::leak(svc);
 }
 
-
-struct ServerConfigurer(Option<Recipient<StopServer>>, Option<String>);
+struct ServerConfigurer {
+    config_path: Option<String>,
+    node_id: NodeId,
+    hub_addr: Option<SocketAddr>,
+}
 
 impl Actor for ServerConfigurer {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut <Self as Actor>::Context) {
-        let config = config::ConfigManager::from_registry();
+        let config = ConfigManager::from_registry();
 
-        println!("path={:?}", &self.1);
-        if let Some(path) = &self.1 {
-            config.do_send(config::SetConfigPath::FsPath(Cow::Owned(path.clone())));
+        println!("path={:?}", &self.config_path);
+        if let Some(path) = &self.config_path {
+            config.do_send(SetConfigPath::FsPath(Cow::Owned(path.clone())));
         }
+
+        let node_id = self.node_id.clone();
+        let hub_addr = self.hub_addr.clone();
 
         ctx.spawn(
             config
-                .send(config::GetConfig::new())
-                .map_err(|e| config::Error::from(e))
+                .send(GetConfig::new())
+                .map_err(|e| ConfigError::from(e))
                 .and_then(|r| r)
                 .map_err(|e| println!("error ! {}", e))
-                .and_then(|c: Arc<ServerConfig>| {
+                .and_then(move |c: Arc<ServerConfig>| {
                     let server = server::new(move || {
                         App::new()
                             .handler("/p2p", p2p_server)
@@ -173,11 +174,23 @@ impl Actor for ServerConfigurer {
                     });
                     let s = server.bind(c.p2p_addr()).unwrap().start();
                     run_mdns_publisher(c.p2p_port);
+
+                    if let Some(hub_addr) = hub_addr {
+                        config.do_send(SetConfig::new(ServerConfig {
+                            hub_addr: Some(hub_addr),
+                            ..(*c).clone()
+                        }));
+                        rpc::ws::start_connection(node_id, hub_addr);
+                    } else if let Some(hub_addr) = c.hub_addr {
+                        rpc::ws::start_connection(node_id, hub_addr);
+                    }
+
                     Ok(())
                 })
                 .into_actor(self)
                 .and_then(|_, _, ctx| fut::ok(ctx.stop())),
         );
+
         println!("configured");
     }
 }
