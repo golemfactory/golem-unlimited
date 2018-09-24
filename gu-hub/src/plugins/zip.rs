f2a8e2c60b0479b8c408e3ecb4654614639f9cbb
destruktiw <hubert.banaszewski@golem.network>
2018-09-24 21:23:09 +0200

    Implement DirectoryParser for plugins

--- gu-hub/src/plugins/zip.rs
+++ gu-hub/src/plugins/zip.rs
@@ -3,40 +3,30 @@ use semver::Version;
 use serde_json;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::fs::read_dir;
+use std::fs::DirEntry;
 use std::fs::File;
+use std::fs::FileType;
+use std::fs::ReadDir;
 use std::io::Read;
 use std::path::Path;
 use std::path::PathBuf;
 use zip::ZipArchive;
 
-pub trait PluginParser: Debug {
-    fn validate_and_load_metadata(
-        path: &Path,
-        gu_version: Version,
-    ) -> Result<(String, PluginMetadata), String>;
-
-    fn load_files(zip_path: &Path, app_name: &String) -> Result<HashMap<PathBuf, Vec<u8>>, String>;
-}
-
-impl<T> PluginParser for T
-where
-    T: PluginParserInner,
-{
+pub trait PluginParser: Debug + PluginParserInner {
     fn validate_and_load_metadata(
         path: &Path,
         gu_version: Version,
     ) -> Result<(String, PluginMetadata), String> {
-        let name = T::validate_package_name(path)?;
-        let metadata = T::load_metadata(path)?;
-        T::validate_gu_version(&metadata, &gu_version)?;
-        T::contains_app_js(&path, &metadata.name())?;
+        let name = Self::validate_package_name(path)?;
+        let metadata = Self::load_metadata(path)?;
+        Self::validate_gu_version(&metadata, &gu_version)?;
+        Self::contains_app_js(&path, &metadata.name())?;
 
         Ok((name, metadata))
     }
 
-    fn load_files(zip_path: &Path, app_name: &String) -> Result<HashMap<PathBuf, Vec<u8>>, String> {
-        T::load_files(zip_path, app_name)
-    }
+    fn load_files(zip_path: &Path, app_name: &String) -> Result<HashMap<PathBuf, Vec<u8>>, String>;
 }
 
 pub trait PluginParserInner: Debug {
@@ -64,8 +54,6 @@ pub trait PluginParserInner: Debug {
             Ok(())
         }
     }
-
-    fn load_files(zip_path: &Path, app_name: &String) -> Result<HashMap<PathBuf, Vec<u8>>, String>;
 }
 
 #[derive(Debug, Default)]
@@ -101,7 +89,9 @@ impl PluginParserInner for ZipParser {
 
         Ok(())
     }
+}
 
+impl PluginParser for ZipParser {
     fn load_files(zip_path: &Path, app_name: &String) -> Result<HashMap<PathBuf, Vec<u8>>, String> {
         let mut archive = Self::open_archive(zip_path)?;
         let mut map = HashMap::new();
@@ -130,8 +120,76 @@ impl PluginParserInner for ZipParser {
     }
 }
 
+#[derive(Debug, Default)]
+pub struct DirectoryParser;
+
+impl DirectoryParser {
+    fn scan_dir(entry: &mut ReadDir, path: &mut PathBuf, map: &mut HashMap<PathBuf, Vec<u8>>) {
+        let mut handle_entry = |(f_type, entry): (FileType, DirEntry)| {
+            path.push(entry.file_name());
+            let _ = if f_type.is_file() {
+                File::open(entry.path())
+                    .as_mut()
+                    .map(|read| map.insert(path.clone(), read_file(read)))
+                    .map_err(|e| warn!("Couldn't read file: {:?}", e));
+            } else if f_type.is_dir() {
+                read_dir(entry.path())
+                    .as_mut()
+                    .map(|read| Self::scan_dir(read, path, map))
+                    .map_err(|e| warn!("Couldn't read directory: {:?}", e));
+            };
+            path.pop();
+
+            Ok(())
+        };
+
+        entry.for_each(|el| {
+            el.and_then(|a| a.file_type().map(|f_type| (f_type, a)))
+                .map_err(|e| format!("Couldn't read filesystem element: {:?}", e))
+                .and_then(&mut handle_entry);
+        })
+    }
+}
+
+impl PluginParserInner for DirectoryParser {
+    fn load_metadata(path: &Path) -> Result<PluginMetadata, String> {
+        let metadata_file = File::open(path.to_path_buf().join("gu-plugin.json"))
+            .map_err(|_| "Couldn't read metadata file".to_string())?;
+
+        Self::parse_metadata(metadata_file)
+    }
+
+    fn contains_app_js(path: &Path, name: &String) -> Result<(), String> {
+        File::open(path.to_path_buf().join(name).join("app.js"))
+            .map_err(|_| "Couldn't read app.js file".to_string())?;
+
+        Ok(())
+    }
+}
+
 fn read_file(file: &mut impl Read) -> Vec<u8> {
     let mut buf = Vec::new();
     file.read_to_end(&mut buf);
     buf
 }
+
+/*
+#[cfg(test)]
+mod tests {
+    use plugins::zip::DirectoryParser;
+    use std::fs::read_dir;
+    use std::collections::HashMap;
+    use std::path::PathBuf;
+
+
+    #[test]
+    fn it_works() {
+        let mut dir = read_dir("/home/hubert/test").unwrap();
+        let mut path = PathBuf::from("");
+        let mut map = HashMap::new();
+        DirectoryParser::scan_dir(&mut dir, &mut path, &mut map);
+
+        println!("{:?}", map)
+    }
+}
+*/
