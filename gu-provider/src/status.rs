c026dd9be6c1069b643cc124cded25dd53fce4b8
Przemysław K. Rekucki <przemyslaw.rekucki@golem.network>
2018-10-02 12:19:17 +0200

    reformat

--- gu-provider/src/status.rs
+++ gu-provider/src/status.rs
@@ -1,9 +1,8 @@
-
-use gu_base::Module;
-use futures::{future, prelude::*};
 use actix::prelude::*;
-use actix_web::{HttpRequest, HttpResponse, HttpMessage, Responder, App, Json, AsyncResponder};
 use actix_web;
+use actix_web::{App, AsyncResponder, HttpMessage, HttpRequest, HttpResponse, Json, Responder};
+use futures::{future, prelude::*};
+use gu_base::Module;
 use std::collections::BTreeMap;
 
 pub fn module() -> impl Module {
@@ -13,7 +12,6 @@ pub fn module() -> impl Module {
 struct StatusModule;
 
 impl Module for StatusModule {
-
     fn decorate_webapp<S: 'static>(&self, app: App<S>) -> App<S> {
         println!("status!");
         app.handler("/status", status_handler)
@@ -22,20 +20,20 @@ impl Module for StatusModule {
 
 #[derive(Serialize)]
 struct StatusBody {
-    envs : BTreeMap<String, EnvStatus>
+    envs: BTreeMap<String, EnvStatus>,
 }
 
-fn status_handler<S : 'static>(_r : &HttpRequest<S>) -> impl Responder {
-    StatusManager::from_registry().send(ListEnvStatus)
+fn status_handler<S: 'static>(_r: &HttpRequest<S>) -> impl Responder {
+    StatusManager::from_registry()
+        .send(ListEnvStatus)
         .map_err(|e| actix_web::error::ErrorInternalServerError(format!("err: {}", e)))
-        .and_then(|envs|
-            match envs {
-                Ok(envs) => Ok(HttpResponse::Ok().json(StatusBody { envs })),
-                Err(e) => Err(actix_web::error::ErrorInternalServerError(format!("err: {}", e)))
-            }
-        )
-        .responder()
-
+        .and_then(|envs| match envs {
+            Ok(envs) => Ok(HttpResponse::Ok().json(StatusBody { envs })),
+            Err(e) => Err(actix_web::error::ErrorInternalServerError(format!(
+                "err: {}",
+                e
+            ))),
+        }).responder()
 }
 
 #[derive(Serialize, Deserialize)]
@@ -43,7 +41,7 @@ pub enum EnvStatus {
     Ready,
     Working,
     Paused,
-    Disabled
+    Disabled,
 }
 
 pub struct GetEnvStatus;
@@ -65,26 +63,28 @@ pub struct AddProvider(&'static str, Recipient<GetEnvStatus>);
 
 impl AddProvider {
     #[inline]
-    pub fn new(name : &'static str, handler : Recipient<GetEnvStatus>) -> AddProvider {
+    pub fn new(name: &'static str, handler: Recipient<GetEnvStatus>) -> AddProvider {
         AddProvider(name, handler)
     }
 }
 
-
 #[derive(Default)]
 pub struct StatusManager {
-    providers : BTreeMap<&'static str, Recipient<GetEnvStatus>>
+    providers: BTreeMap<&'static str, Recipient<GetEnvStatus>>,
 }
 
 impl Actor for StatusManager {
     type Context = Context<Self>;
-
 }
 
 impl Handler<AddProvider> for StatusManager {
     type Result = ();
 
-    fn handle(&mut self, msg: AddProvider, ctx: &mut Self::Context) -> <Self as Handler<AddProvider>>::Result {
+    fn handle(
+        &mut self,
+        msg: AddProvider,
+        ctx: &mut Self::Context,
+    ) -> <Self as Handler<AddProvider>>::Result {
         self.providers.insert(msg.0, msg.1);
     }
 }
@@ -92,17 +92,20 @@ impl Handler<AddProvider> for StatusManager {
 impl Handler<ListEnvStatus> for StatusManager {
     type Result = ActorResponse<StatusManager, BTreeMap<String, EnvStatus>, String>;
 
-    fn handle(&mut self, msg : ListEnvStatus, ctx : &mut Self::Context) -> Self::Result {
+    fn handle(&mut self, msg: ListEnvStatus, ctx: &mut Self::Context) -> Self::Result {
         ActorResponse::async(
-        future::join_all(
-        self.providers.clone().into_iter().map(move |(env_name, env_addr)| {
-            let name = env_name.to_string();
-            env_addr.send(GetEnvStatus).and_then(move |s| Ok((name, s)))
-        })).and_then(|envs| -> Result<BTreeMap<String, EnvStatus>, actix::MailboxError> {
-            Ok(envs.into_iter().collect())
-        })
-            .map_err(|e| format!("{}", e))
-            .into_actor(self))
+            future::join_all(self.providers.clone().into_iter().map(
+                move |(env_name, env_addr)| {
+                    let name = env_name.to_string();
+                    env_addr.send(GetEnvStatus).and_then(move |s| Ok((name, s)))
+                },
+            )).and_then(
+                |envs| -> Result<BTreeMap<String, EnvStatus>, actix::MailboxError> {
+                    Ok(envs.into_iter().collect())
+                },
+            ).map_err(|e| format!("{}", e))
+            .into_actor(self),
+        )
     }
 }
 
