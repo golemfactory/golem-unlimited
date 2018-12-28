a137ccbebaf461d128f222e7baf79fb45d96b0c8
Przemysław K. Rekucki <przemyslaw.rekucki@golem.network>
2018-12-28 21:44:10 +0100

    WIP async_tar

--- gu-provider/src/async_tar.rs
+++ gu-provider/src/async_tar.rs
@@ -1,1 +1,60 @@
 
+use futures::prelude::*;
+use futures::future;
+
+use std::io;
+use std::cmp::min;
+use bytes::Bytes;
+use tar::Archive;
+
+pub struct FakeBuffer {
+    bytes : Vec<u8>,
+    eof: bool
+}
+
+impl FakeBuffer {
+    fn append(&mut self, bytes : &Bytes) {
+        self.bytes.extend_from_slice(bytes.as_ref())
+    }
+
+    fn close(&mut self) {
+        self.eof = true;
+    }
+}
+
+impl io::Read for FakeBuffer {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
+        let len = if self.eof {
+            min(self.bytes.len(), buf.len())
+        } else {
+            buf.len()
+        };
+
+        if len == 0 {
+            return Ok(0)
+        }
+
+        if self.bytes.len() <= buf.len() {
+            buf[0..len].copy_from_slice(&self.bytes[0..len]);
+            let _ = self.bytes.drain(0..len);
+            Ok(len)
+        }
+        else {
+            Err(io::ErrorKind::WouldBlock.into())
+        }
+    }
+}
+
+pub struct EntryStream<T> {
+    upstream  : T,
+    archive : Archive<FakeBuffer>,
+}
+
+impl<'a, T : Stream<Item=Bytes>> Stream for &'a mut EntryStream<T> {
+    type Item = tar::Entry<'a, FakeBuffer>;
+    type Error = ();
+
+    fn poll(&mut self) -> Result<Async<Option<<Self as Stream>::Item>>, <Self as Stream>::Error> {
+        unimplemented!()
+    }
+}
